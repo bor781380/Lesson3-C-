362b3edc30f3e1f49913b99a50f3dba1acf07c33
Andrey <avgust1@mail.ru>
2023-11-11 20:37:35 +0500

    add Task5 dz

---
+++ Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿//генерация рандомных чисел в массив
+Random rnd = new Random();//Random это тип данных как int
+
+int size = 1000; //задали размер массива
+int[] arr_int = new int[size]; //описали массив
+int i = 0;
+while (i < size)
+{
+    arr_int[i] = rnd.Next(1, 10); //заполняем массив случайными данными от 1 до 9
+    i++;
+}
+
+i = 0;
+while (i < size)
+{
+    Console.Write($"{arr_int[i]} "); //вывод массива на печать
+    i++;
+}
