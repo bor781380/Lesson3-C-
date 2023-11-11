af10cf45d780cf19e0b03e8b81b4f93e94870d25
Andrey <avgust1@mail.ru>
2023-11-11 20:57:22 +0500

    add Task6 dz

---
+++ Task6/Program.cs
@@ -0,0 +1,21 @@
+﻿int size = 5;
+int[] arr_int = new int[size];
+int i=0;
+while(i<size)
+{
+    Console.WriteLine("Введите элемент массива " + (i+1) +"-е целое число: ");
+    string input = Console.ReadLine();
+    arr_int[i] = Convert.ToInt32(input);
+    i++;
+}
+PrintArray(arr_int);
+
+void PrintArray(int[] arr_int)
+{
+    int i = 0;
+    while (i < size)
+    {
+        Console.Write($"{arr_int[i]} ");
+        i++;
+    }
+}
