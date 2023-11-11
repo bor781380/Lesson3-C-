252521fa4769146d7e04c89618716bf595d5f42e
Andrey <avgust1@mail.ru>
2023-11-11 18:21:46 +0500

    add lesson3 dz

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿double CalkulateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denumenator = c + d;
+    double recult = numenator / denumenator;
+    return recult;
+}
+double answer=CalkulateFormula(1, 2, 3, 4);
+Console.WriteLine(answer);
